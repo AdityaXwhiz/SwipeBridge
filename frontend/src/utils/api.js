a7c7b88e04368145a81da60fc68e6f5a5d067bcc
Aditya Singh <adityasingh@Adityas-MacBook-Pro.local>
2026-04-18 23:39:34 +0530

    improved final version

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: BASE_URL + '/api',
   headers: { 'Content-Type': 'application/json' },
 })
 
